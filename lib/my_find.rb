a0bc9840cda781e3f28738ea796c2f31db19c0ae
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:25:06 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  idx = 0
+  find_result = []
+  while collection[idx]
+    if yield(collection[idx])
+      return collection[idx]
+    end
+    idx+=1
+  end
 end
